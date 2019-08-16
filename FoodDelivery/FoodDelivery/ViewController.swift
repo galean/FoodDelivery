43c2486ec6346fc2b3b81b9da758163af54e73df
Andrew Plotnikov <Galean.pal@gmail.com>
2019-08-16 13:19:09 +0300

    Created project

---
+++ FoodDelivery/FoodDelivery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodDelivery
+//
+//  Created by Galean Pallerman on 16.08.2019.
+//  Copyright © 2019 GPco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
